71a41b5fdea13b0729f8a86c07c368d9cf33ca0f
Spencer Pingitore <silentfoe6@gmail.com>
2023-12-13 10:36:50 -0500

    first attempt to solve with python and better solution than mine I found on the code wars solutions page

---
+++ fundamentals/is_he_gonna_survive.py
@@ -0,0 +1,17 @@
+# A hero is on his way to the castle to complete his mission. However, he's been told that the castle is surrounded with a couple of powerful dragons! each dragon takes 2 bullets to be defeated, our hero has no idea how many bullets he should carry.. Assuming he's gonna grab a specific given number of bullets and move forward to fight another specific given number of dragons, will he survive?
+
+# Return true if yes, false otherwise :)
+
+
+# my solution: 
+
+def hero(bullets, dragons):
+    if dragons * 2 > bullets: 
+        return False
+    
+    return True
+
+# better solution on the solutions page
+
+def hero(bullets, dragons):
+    return bullets >= dragons * 2
