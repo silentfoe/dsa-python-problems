bf88d24b8e23a7097da0a269083532d2c3a4209d
Spencer Pingitore <silentfoe6@gmail.com>
2023-12-17 10:33:56 -0500

    first attempt to solve with python and cool solution I found on the code wars solutions page

---
+++ fundamentals/descending_order.py
@@ -0,0 +1,23 @@
+# Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+# Examples:
+# Input: 42145 Output: 54421
+
+# Input: 145263 Output: 654321
+
+# Input: 123456789 Output: 987654321
+
+# my solution: 
+
+def descending_order(num):
+
+    return int(''.join(sorted(str(num), reverse=True)))
+
+
+# cool solution that takes into account inputs that are not integers: 
+
+def Descending_Order(num):
+    if isinstance(num, int) and num >= 0:
+        return int(''.join(sorted(str(num),reverse=True)))
+    else:
+        raise ValueError('Non-negative integer expected')
