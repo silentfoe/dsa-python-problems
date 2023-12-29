fbaa5e542012d5767fd9cfd76dd872d3982fdc15
Spencer Pingitore <silentfoe6@gmail.com>
2023-12-28 19:25:38 -0500

    first attempt to solve with python and two other solutions that were much shorter than my solution

--- fundamentals/mumbling.py
+++ fundamentals/mumbling.py
@@ -9,3 +9,39 @@
 
 # my solution: 
 
+def accum(s):
+    
+    #create an empty list
+    letters = []
+    
+    #make the string lower case
+    split = s.lower()
+    
+    #want to start a loop for each character that duplicates the char based on the index number plus 1
+    #start by looping over each index
+    for indx in range(len(split)):
+        chars = ''
+        
+        #duplicating each letter found at the index to total the index + 1, i.e. index 0 would have 1 letter
+        for nums in range(indx + 1):
+            #checking to see if the char is not the first, if so then it's lower case           
+            if not nums == 0:
+                chars += split[indx]
+            #if the char is the first, capitilize it
+            else:
+                chars += split[indx].upper()
+            
+        letters.append(chars)
+    
+    return '-'.join(letters)
+
+
+# other solutions that are much shorter found on the solutions page: 
+
+#1
+def accum(s):
+    return '-'.join(c.upper() + c.lower() * i for i, c in enumerate(s))
+
+#2
+def accum(s):
+    return '-'.join((a * i).title() for i, a in enumerate(s, 1))
