cb954e188372ea0dea98bc8c3e314d3cf1cf4309
Spencer Pingitore <silentfoe6@gmail.com>
2024-01-05 10:41:38 -0500

    first attempt to solve using python along with 3 solutions I thought were good to know found on the solutions page

---
+++ fundamentals/binary_addition.py
@@ -0,0 +1,28 @@
+# Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+
+# The binary number returned should be a string.
+
+# Examples:(Input1, Input2 --> Output (explanation)))
+
+# 1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
+# 5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
+
+# my solution: 
+
+def add_binary(a,b):
+   return bin(a + b)[2::]
+
+
+# three other cool solutions I found on the solutions page: 
+
+#1
+def add_binary(a,b):
+    return '{0:b}'.format(a + b)
+
+#2
+def add_binary(a, b):
+    return format(a + b, 'b')
+
+#3
+def add_binary(a,b):
+    return f"{a + b:b}"
