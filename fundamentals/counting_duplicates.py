6b5d6582b28d74ffc019b4eef5165fba3a8ecd52
Spencer Pingitore <silentfoe6@gmail.com>
2024-01-30 21:00:33 -0500

    first attempt to solve with python along with better time complexity solution found on the solutions page

---
+++ fundamentals/counting_duplicates.py
@@ -0,0 +1,43 @@
+# Count the number of Duplicates
+# Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+# Example
+# "abcde" -> 0 # no characters repeats more than once
+# "aabbcde" -> 2 # 'a' and 'b'
+# "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+# "indivisibility" -> 1 # 'i' occurs six times
+# "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+# "aA11" -> 2 # 'a' and '1'
+# "ABBA" -> 2 # 'A' and 'B' each occur twice
+
+
+# my solution: 
+
+def duplicate_count(text):
+    
+    multiple_chars = {}
+    count = 0
+    
+    for char in text.lower():
+        if char in multiple_chars:
+            multiple_chars[char] += 1
+        else:
+            multiple_chars[char] = 1
+            
+    for letters in multiple_chars:
+        if multiple_chars[letters] > 1:
+            count += 1
+    
+    return count
+
+# better time complexity solution I found on the code wars solutions page: 
+
+def duplicate_count(text):
+    seen = set()
+    dupes = set()
+    for char in text:
+        char = char.lower()
+        if char in seen:
+            dupes.add(char)
+        seen.add(char)
+    return len(dupes)
