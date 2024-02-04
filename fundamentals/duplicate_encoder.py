8412551973e3d6ee7d35e09de08c292447b06e39
Spencer Pingitore <silentfoe6@gmail.com>
2024-02-03 20:14:56 -0500

    first attempt to solve with python along with clever solution found on the solutions page

---
+++ fundamentals/duplicate_encoder.py
@@ -0,0 +1,30 @@
+# The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+# Examples
+# "din"      =>  "((("
+# "recede"   =>  "()()()"
+# "Success"  =>  ")())())"
+# "(( @"     =>  "))((" 
+# Notes
+# Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
+
+# my solution: 
+
+def duplicate_encode(word):
+    
+    string = ''
+    low_case = word.lower()
+    
+    for char in low_case:
+        if low_case.count(char) > 1:
+            string += ')'
+        else:
+            string += '('
+    
+    return string
+
+
+# solution I thought was clever from the solutions page: 
+
+def duplicate_encode(word):
+    return "".join(["(" if word.lower().count(c) == 1 else ")" for c in word.lower()])
