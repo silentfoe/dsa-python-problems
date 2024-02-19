d7b2769debd946bd3978736c7133c1c66d9efb45
Spencer Pingitore <silentfoe6@gmail.com>
2024-02-18 20:57:38 -0500

    first attempt to solve with python along with a couple of clever solutions found on the solutions page

--- fundamentals/break_camel_case.py
+++ fundamentals/break_camel_case.py
@@ -4,3 +4,33 @@
 # "camelCasing"  =>  "camel Casing"
 # "identifier"   =>  "identifier"
 # ""             =>  ""
+
+# my solution: 
+
+def solution(s):
+    
+    sentence = ''
+    
+    for indx in range(len(s)):
+        if s[indx] == s[indx].upper():
+            sentence += f' {s[indx]}'
+        else:
+            sentence += s[indx]
+    
+    return sentence
+
+# a couple of clever solutions I found on the solutions page: 
+
+#1
+def solution(s):
+    return ''.join(' ' + c if c.isupper() else c for c in s)
+
+
+#2
+def solution(s):
+    newStr = ""
+    for letter in s:
+        if letter.isupper():
+            newStr += " "
+        newStr += letter
+    return newStr
